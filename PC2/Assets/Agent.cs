4643dca73f92135b7f12197f55d5e82cc916cd9e
MoyraRodriguez <moyrarodriguez@gmail.com>
2019-09-30 21:44:06 -0500

    practica

---
+++ PC2/Assets/Agent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Agent : SBAgent
+{
+    public Transform target;
+    public Transform target2;
+
+    void Start()
+    {
+        maxSpeed = 10f;
+        maxSteer = 1f;
+        target2 = GameObject.Find("Target2").transform;
+    }
+
+    void Update()
+    {
+        //velocity += SteeringBehaviours.InsideCircle(this,target,target2,3); esto de aqui hace que funcione el circulo
+        velocity += SteeringBehaviours.InsideRectangle(this, target, target2, 2, 2);// esto hace que funcione el cuadrado
+
+
+        transform.position += velocity * Time.deltaTime;
+    }
+}
---
+++ PC2/Assets/Limiter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limiter : MonoBehaviour
+{
+    [Range(1, 20)]
+    [SerializeField]
+    private int SizeY = 5;
+    private float SizeX;
+
+    [SerializeField]
+    private Transform Target;
+    [SerializeField]
+    private Transform Follower;
+
+    private void Awake()
+    {
+        SizeX = SizeY * (16f / 9f);
+    }
+
+    private void Update()
+    {
+        /*float posX = Target.position.x;
+        if(posX < -SizeX)
+        {
+            posX = -SizeX;
+        }
+        if (posX > SizeX)
+        {
+            posX = SizeX;
+        }
+
+        float posY = Target.position.y;
+        if (posY < -SizeY)
+        {
+            posY = -SizeY;
+        }
+        if (posY > SizeY)
+        {
+            posY = SizeY;
+        }
+
+        Follower.position = new Vector3(posX, posY, -10);*/
+
+        Follower.position = new Vector3(
+            Mathf.Clamp(Target.position.x, -SizeX, SizeX),
+            Mathf.Clamp(Target.position.y, -SizeY, SizeY),
+            -10);
+    }
+
+    private void OnDrawGizmos()
+    {
+        float h = SizeY * 2;
+        float w = h * (16f / 9f);
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(transform.position, new Vector3(w, h));
+    }
+}
